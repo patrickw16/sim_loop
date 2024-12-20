60347e6ee583288140e7e79ffb4411f70193d4d9
Patrick Weissensteiner <p_weissensteiner@icloud.com>
2024-12-20 17:01:59 +0100

    adapted parameter set

--- colab_execution.py
+++ colab_execution.py
@@ -49,7 +49,6 @@ def calculate_distance_threshold(distances, dt, j, ego_deceleration):
         return np.square(delta_v) / (2 * ego_deceleration)
 
 
-
 xosc_path = "/content/sim_loop/scenarios/cut-in.xosc"
 lib_path = "/content/esmini"
 
@@ -174,7 +173,7 @@ while se.SE_GetQuitFlag() == 0 and se.SE_GetSimulationTime() < 17.0:
         #results = model(f"images/{image_name}")
         #cv2_imshow(img_array)
         results = model(img_array)
-        #results[0].save()
+        results[0].save()
 
         # Iterate through the results and calculate distances
         for r in results:
